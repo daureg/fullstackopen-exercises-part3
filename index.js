a4ab08e640510191881783fefdebfb3513be5905
Géraud Le Falher <daureg@gmail.com>
2020-05-12 23:31:00 +0200

    3.4 add deletion endpoint

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(note => note.id !== id)
+  res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
   lines = [
     `<p>Phonebook has info for ${persons.length} people</p>`,
