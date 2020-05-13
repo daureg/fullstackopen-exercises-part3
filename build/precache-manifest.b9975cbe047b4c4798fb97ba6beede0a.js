6ba69267a63fbbe9039e89e3c9cd4d3480d863a2
Géraud Le Falher <daureg@gmail.com>
2020-05-14 00:18:01 +0200

    3.10 serve frontend from here

---
+++ build/precache-manifest.b9975cbe047b4c4798fb97ba6beede0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc149a53d8e72c35916a637c3da2053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65670b4a8381b0c68063",
+    "url": "/static/css/main.c520b617.chunk.css"
+  },
+  {
+    "revision": "da9c83f8379307f89c63",
+    "url": "/static/js/2.6de0d325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6de0d325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65670b4a8381b0c68063",
+    "url": "/static/js/main.67590d53.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
