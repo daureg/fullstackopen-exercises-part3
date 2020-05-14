f53af9203f27a9facee35c1251b7a722a35a77fe
Géraud Le Falher <daureg@gmail.com>
2020-05-14 21:00:08 +0200

    3.12 command line to mongoDB

---
+++ mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://sherlock:${password}@cluster0-akmig.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if ( process.argv.length>3 ) {
+  name = process.argv[3]
+  number = process.argv[4]
+  const person = new Person({ name, number })
+
+  person.save().then(response => {
+    console.log(`Added ${response.name} (${response.number}) to the phonebook`)
+    mongoose.connection.close()
+  })
+}
+else {
+Person.find({}).then(result => {
+  console.log("phonebook:")
+  result.forEach(p => { console.log(`${p.name} (${p.number})`) })
+  mongoose.connection.close()
+})
+}
