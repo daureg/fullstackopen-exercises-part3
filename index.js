f944d6c4a4c2ef14795cc2bff8d3808dc8f2b9b8
Géraud Le Falher <daureg@gmail.com>
2020-05-16 00:59:01 +0200

    3.13 & 3.14 use mongo to get and post new person

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -9,41 +11,14 @@ app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
-let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    }
-]
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => res.json(persons))
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = persons.find(note => note.id === id)
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
+  Person.findById(req.params.id)
+    .then(person => { res.json(person) })
+    .catch(error => {console.error(error); res.status(404).end()})
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -53,11 +28,15 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-  lines = [
-    `<p>Phonebook has info for ${persons.length} people</p>`,
-    `<p>${(new Date()).toString()}</p>`
-  ]
-  res.send(lines.join("\n"))
+  Person.estimatedDocumentCount()
+    .then(num_people => {
+      lines = [
+        `<p>Phonebook has info for ${num_people} people</p>`,
+        `<p>${(new Date()).toString()}</p>`
+      ]
+      res.send(lines.join("\n"))
+    })
+    .catch(error => {console.error(error); response.status(404).end()})
 })
 
 const generateId = () => Math.floor(Math.random() * Math.floor(4000000000))
@@ -71,22 +50,22 @@ app.post('/api/persons', (req, res) => {
       return res.status(400).json({error: `You must provide a '${field}' for that person`})
     }
   }
+  /*
   if (persons.filter(p => p.name.toLowerCase() === body.name.toLowerCase()).length > 0) {
       return res.status(400).json({error: `There is already a person named '${body.name}' in the phone book`})
   }
+  */
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
-
-  persons = persons.concat(person)
-
-  res.json(person)
+  })
+  person.save()
+    .then(savedPerson => { res.json(savedPerson) })
+    .catch(error => {console.error(error); response.status(404).end()})
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
