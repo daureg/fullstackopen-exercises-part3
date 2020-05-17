bf25f5fc2b7b978230fe9e8b3a3b3e3848352077
Géraud Le Falher <daureg@gmail.com>
2020-05-17 12:45:50 +0200

    3.17 & 3.18 add update endpoint

--- index.js
+++ index.js
@@ -57,6 +57,17 @@ app.post('/api/persons', (req, res, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => res.json(updatedPerson))
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (req, res) => {
   res.status(404).send({error: 'unknown endpoint'})
 }
